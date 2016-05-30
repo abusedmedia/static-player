793febef6ff2efed6ab5c7eb14a4d7e6472db505
abusedmedia <ff@todo.to.it>
2016-05-30 09:50:46 +0200

    cleanup

--- static-player.js
+++ static-player.js
@@ -1,64 +1,118 @@
-(function () {
-	'use strict';
+window.StaticPlayer = (function (window, undefined) {
 
+	var instance = null;
 
-	function parse(){
+	$.holdReady(true)
 
-		console.log('parse')
-
-		var fragments = $("[process]")
-
-		if(fragments.length>0) $.holdReady(true)
+	function initializeNewModule() {
 		
-		var count = 0
+		function parse(_selection, _data){
 
-		fragments.each(function () {
-			var self = this
-			var path = $(self).attr('process')
-			var compiled 
-			var processed
+			console.log('---------parse')
 
-			if(path){
-				$.get(path, function(result){
-					compiled = _.template(result)
-					processed = compiled(window.data)
-
-					finalize(self, processed, true)
-
-					count++
-					if(count >= fragments.length) $.holdReady(false)
-
-					if($(processed).find("[process]")){
-						parse();
-					}
-				})
-			}else{
-				var cnt = $(self).html().replace(/&lt;/g, '<').replace(/&gt;/g, '>')
-				compiled = _.template(cnt)
-				processed = compiled(window.data)
-				finalize(self, processed, true)
-			}
-
-			function finalize(self, processed, remove){
-				var prev = $(self).prev()
-				if(prev.length>0){
-					var newfrag = prev.after(processed)
-				}else{
-					var newfrag = $(self).parent().prepend(processed)
-				}
-				$(self).removeAttr('process')
-				if(remove) $(self).remove()
-			}
-
+			if(_selection.length==0) $.holdReady(false)
 			
-		})
+			var count = 0
 
+			_selection.each(function () {
+				var self = this
+
+				var path = $(self).attr('process')
+				$(self).removeAttr('process')
+
+				if(path){
+					function ldr(__self, path, _data){
+						console.log('include', path)
+						$.get(path, function(result){
+							var model = $(__self).attr('model')
+							var data = (model) ? _data[model] : _data
+							console.log('included', path, data)
+							var compiled = _.template(result)
+							var processed = compiled(data)
+
+							var newfrag = finalize(__self, processed, true)
+							
+							count++
+							if(count >= _selection.length) $.holdReady(false)
+
+							setTimeout(function(){
+								var match = newfrag.match(/process/g)
+								if(match && match.length>0){
+									parse( $("[process]"), data );
+								}
+							})
+						})
+					}
+					ldr(self, path, _data)
+				}else{
+					console.log('process')
+					var cnt = $(self).html().replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+					var model = $(self).attr('model')
+					var data = (model) ? _data[model] : _data
+					var compiled = _.template(cnt)
+					var processed = compiled(data)
+					finalize(self, processed, true)
+				}
+
+				function finalize(self, processed, remove){
+					var prev = $(self).prev()
+					if(prev.length>0){
+						var newfrag = prev.after(processed)
+					}else{
+						var newfrag = $(self).parent().prepend(processed)
+					}
+					if(remove) $(self).remove()
+
+					return processed			
+				}
+
+				
+			})
+
+		}
+
+
+		
+
+		var manualBootstrap = false;
+
+		var begin = function(tag, global){
+			console.log('begin')
+			manualBootstrap = true
+			var _tag = (tag) ? tag : 'process'
+			var _glob = (global) ? global : window
+			parse( $("["+_tag+"]"), _glob );
+		}
+
+		if($('[process-init]').length>0){
+			begin()
+		}
+
+		return {
+	      begin : begin
+	    };
+	    
 	}
 
+	if($('[process-init]').length>0){
+		console.log('auto')
+		instance = new initializeNewModule();
+	}
 
-	$(document).ready(function(){
-		parse();
-	})
-	
+ 
+    function getInstance() {
+	    if( ! instance ) {
+	      instance = new initializeNewModule();
+	    }
+	    return instance;
+	}
+	  
+	return {
+	    getInstance : getInstance
+	};
 
-})();
+
+
+
+})(window);
+
